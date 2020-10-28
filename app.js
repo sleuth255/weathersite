78c1b445a4b39336598e1e3427ba26b2cd495945
sleuth255 <sleuth255@hotmail.com>
2020-10-28 14:19:46 -0500

    More current conditions items

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ function makeCompassVector(direction){
 	var heading='';
     switch(true){
     case (direction == 0):
-    	   heading='';
            break;
        case (direction < 22.5):
  	       heading='N';left=220;top=100;rotation=0;
@@ -82,12 +81,12 @@ function makeCompassVector(direction){
     	   heading='NNW';left=182;top=125;rotation=337.5;
     	   break;
        default:
-    	   heading='';left=top=rotation=0;
            break;
     }
     return {heading: heading,left: left,top: top,rotation: rotation};
-}/***************************************************************/
-
+}
+/***************************************************************/
+// globals
 let cookie = {};
 var direction = 0;
 var speed = 0;
@@ -95,13 +94,22 @@ var gustSpeed = 0;
 var gustDirection = 0;
 var avgSpeed = 0
 var avgDirection = 0
-var currentConditions;
+var inTemp = 0;
+var inHum = 0;
+var outTemp = 0;
+var outHum = 0;
+var outDewPt = 0;
+var outWindChill = 0;
+var outHeatIdx = 0;
+var inBarometer = 0;
+var inBarometerTrend;
+
 //process...
 app.get('/', function (req, res) {
-    res.render('defaultresponse',{avgSpeed: Math.floor(avgSpeed),avgDirection: makeCompassVector(avgDirection).heading,gustSpeed: gustSpeed,gustDirection: makeCompassVector(gustDirection).heading})
+    res.render('defaultresponse',{speed:speed,left:makeCompassVector(direction).left,top:makeCompassVector(direction).top,rotation:makeCompassVector(direction).rotation,inBarometer: inBarometer,inBarometerTrend: inBarometerTrend,outWindChill, outWindChill, outHeatIdx: outHeatIdx,inTemp: inTemp, inHum: inHum, outTemp: outTemp, outHum: outHum, outDewPt: outDewPt,avgSpeed: avgSpeed,avgDirection: makeCompassVector(avgDirection).heading,gustSpeed: gustSpeed,gustDirection: makeCompassVector(gustDirection).heading})
 })
 app.get('/liveconditions', function (req, res) {
-    res.render('liveconditions',{avgSpeed: Math.floor(avgSpeed),avgDirection: makeCompassVector(avgDirection).heading,gustSpeed: gustSpeed,gustDirection: makeCompassVector(gustDirection).heading})
+    res.render('liveconditions',{inBarometer: inBarometer,inBarometerTrend: inBarometerTrend,outWindChill, outWindChill, outHeatIdx: outHeatIdx,inTemp: inTemp, inHum: inHum, outTemp: outTemp, outHum: outHum, outDewPt: outDewPt,avgSpeed: avgSpeed,avgDirection: makeCompassVector(avgDirection).heading,gustSpeed: gustSpeed,gustDirection: makeCompassVector(gustDirection).heading})
 })
 app.get('/livewind', function (req, res) {
     res.locals.err = false;
@@ -121,19 +129,17 @@ server.on('listening',function(){
   console.log('UDP Server is : ' + family);
 });
 server.on('message',function(msg,info){
-	  //console.log('Data received from client : ' + msg.toString());
-	  //console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
 	  var obj = JSON.parse(msg);
 	  direction=obj.conditions[0].wind_dir_last;
 	  speed=obj.conditions[0].wind_speed_last;
 	  gustDirection=obj.conditions[0].wind_dir_at_hi_speed_last_10_min;
 	  gustSpeed=obj.conditions[0].wind_speed_hi_last_10_min;
-	  //console.log("Wind Direction: "+direction)
 });
 
 
 //Tell WLL to start send live data every 5 minutes and repeat request every 5 minutes
 
+console.log(app.locals.moment(Date.now()).format('MM/DD/YY h:mm:ss a')+': Retrieving current conditions')
 http.get('http://weatherlinklive.tpg/v1/current_conditions',function(resp){
 	data = '';
 	resp.on('data',function(chunk){
@@ -142,15 +148,31 @@ http.get('http://weatherlinklive.tpg/v1/current_conditions',function(resp){
 	resp.on('end',function(){
 		console.log(data.toString())
 		var obj = JSON.parse(data);
-		avgSpeed = obj.data.conditions[0].wind_speed_avg_last_10_min;
-		avgDirection = obj.data.conditions[0].wind_dir_scalar_avg_last_10_min;
+		avgSpeed = Math.round(obj.data.conditions[0].wind_speed_avg_last_10_min);
+		avgDirection = Math.round(obj.data.conditions[0].wind_dir_scalar_avg_last_10_min);
+		inTemp = Math.round(obj.data.conditions[2].temp_in);
+		inHum = Math.round(obj.data.conditions[2].hum_in);
+		outTemp = Math.round(obj.data.conditions[0].temp);
+		outHum = Math.round(obj.data.conditions[0].hum);
+		outDewPt = Math.round(obj.data.conditions[0].dew_point);
+		outWindChill = Math.round(obj.data.conditions[0].wind_chill);
+		outHeatIdx = Math.round(obj.data.conditions[0].heat_index);
+		inBarometer = Math.round((obj.data.conditions[3].bar_sea_level)*100)/100
+		inBarometerTrend = obj.data.conditions[3].bar_trend
+		if (inBarometerTrend < 0)
+			inBarometerTrend = 'Falling'
+		else
+		if (inBarometerTrend > 0)
+			inBarometerTrend = 'Rising'
+		else
+			inBarometerTrend = 'Steady'
 		http.get('http://weatherlinklive.tpg/v1/real_time?duration=300',function(resp){
 			data = '';
 			resp.on('data',function(chunk){
 				data+=chunk
 			})
 			resp.on('end',function(){
-				console.log(data.toString())
+				//console.log(data.toString())
 			})
 		})
 	})
@@ -159,6 +181,7 @@ http.get('http://weatherlinklive.tpg/v1/current_conditions',function(resp){
 
 setInterval(function(){
 	try{
+	   console.log(app.locals.moment(Date.now()).format('MM/DD/YY h:mm:ss a')+': Retrieving current conditions')
 	   http.get('http://weatherlinklive.tpg/v1/current_conditions',function(resp){
 		   data = '';
 		   resp.on('data',function(chunk){
@@ -168,6 +191,22 @@ setInterval(function(){
 			   var obj = JSON.parse(data);
 			   avgSpeed = obj.data.conditions[0].wind_speed_avg_last_10_min;
 			   avgDirection = obj.data.conditions[0].wind_dir_scalar_avg_last_10_min;
+			   inTemp = Math.round(obj.data.conditions[2].temp_in);
+			   inHum = Math.round(obj.data.conditions[2].hum_in);
+			   outTemp = Math.round(obj.data.conditions[0].temp);
+			   outHum = Math.round(obj.data.conditions[0].hum);
+			   outDewPt = Math.round(obj.data.conditions[0].dew_point);
+			   outWindChill = Math.round(obj.data.conditions[0].wind_chill);
+			   outHeatIdx = Math.round(obj.data.conditions[0].heat_index);
+			   inBarometer = Math.round((obj.data.conditions[3].bar_sea_level)*100)/100
+			   inBarometerTrend = obj.data.conditions[3].bar_trend
+				if (inBarometerTrend < 0)
+					inBarometerTrend = 'Falling'
+				else
+				if (inBarometerTrend > 0)
+					inBarometerTrend = 'Rising'
+				else
+					inBarometerTrend = 'Steady'
 			   try{
 			      http.get('http://weatherlinklive.tpg/v1/real_time?duration=300',function(resp){
 				      data = '';
@@ -175,7 +214,7 @@ setInterval(function(){
 					      data+=chunk
 				      })
 				      resp.on('end',function(){
-					      console.log(data.toString())
+					      //console.log(data.toString())
 				      })
 			      })
 			   }
